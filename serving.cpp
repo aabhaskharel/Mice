aaf44364d5f39bbb9c0bb826a4f10a7c88ffe29d
slamsal <safal.lamsal@mavs.uta.edu>
2017-11-22 23:12:58 -0600

    added load constructur and save method to serving

--- serving.cpp
+++ serving.cpp
@@ -1,12 +1,35 @@
-#include <vector>
 #include <iostream>
 #include <iomanip>
+#include <stdexcept>
+
 #include "serving.h"
 
 using namespace std;
 
 //constructor
 Serving::Serving(Containr container): _container{container} {}
+Serving::Serving(std::istream& ist) {
+    // The header must have been stripped from the incoming stream at this point
+    std::string header1, header2;
+    while (true) {
+        std::getline(ist, header1); // header
+        std::getline(ist, header2);
+        if (header1 != "#") throw std::runtime_error("missing # during input");
+        if (header2 == "END SERVING") break;  // footer
+        else if (header2 == "CONTAINER") _container = Containr{ist};
+        else if (header2 == "FLAVOR") _flavors.push_back(Flavor{ist});
+        else if (header2 == "TOPPING") _toppings.push_back(Topping{ist});
+        else throw std::runtime_error("invalid item type in Serving");
+    }
+}
+
+void Serving::save(std::ostream& ost) {
+    ost << "#" << std::endl << "SERVING" << std::endl; // header
+    _container.save(ost);
+    for (Flavor& f : _flavors) f.save(ost);
+    for (Topping& t : _toppings) t.save(ost);
+    ost << "#" << std::endl << "END SERVING" << std::endl; // footer
+}
 
 //return container
 Containr Serving::get_container() {return _container;}
@@ -14,7 +37,7 @@ Containr Serving::get_container() {return _container;}
 //return flavors
 vector<Flavor> Serving::get_flavors() {return _flavors;}
 vector<int> Serving::get_top_kind() {return _top_kind;}
-		
+
 //return toppings
 vector<Topping> Serving::get_toppings() {return _toppings;}
 
@@ -33,46 +56,46 @@ void Serving::set_flavor(Flavor flavor) {
 double Serving::get_total_retail_price() {
 		double total=0; double f=0; double t;
 		total = _container.get_retail_price();
-		
+
 		for(int i=0; i<_flavors.size(); i++){
-				f+=_flavors[i].get_retail_price();	
+				f+=_flavors[i].get_retail_price();
 		}
-		
+
 		for(int i=0; i<_toppings.size(); i++){
 			switch (_top_kind[i])
 			{
 			case 0:	t+=_toppings[i].get_retail_price()*1; break;
 			case 1:	t+=_toppings[i].get_retail_price()*2; break;
 			case 2:	t+=_toppings[i].get_retail_price()*3; break;
-			case 3:	t+=_toppings[i].get_retail_price()*4; break;	
+			case 3:	t+=_toppings[i].get_retail_price()*4; break;
 			}
 		}
-		
+
 	total = total + f + t;
-	
+
 	return total;
 }
 
 double Serving::get_total_wholesale_price() {
 		double total=0; double f=0; double t;
 		total = _container.get_wholesale_price();
-		
+
 		for(int i=0; i<_flavors.size(); i++){
-				f+=_flavors[i].get_wholesale_price();	
+				f+=_flavors[i].get_wholesale_price();
 		}
-		
+
 		for(int i=0; i<_toppings.size(); i++){
 			switch (_top_kind[i])
 			{
 			case 0:	t+=_toppings[i].get_wholesale_price()*1; break;
 			case 1:	t+=_toppings[i].get_wholesale_price()*2; break;
 			case 2:	t+=_toppings[i].get_wholesale_price()*3; break;
-			case 3:	t+=_toppings[i].get_wholesale_price()*4; break;	
+			case 3:	t+=_toppings[i].get_wholesale_price()*4; break;
 			}
 		}
-		
+
 	total = total + f + t;
-	
+
 	return total;
 }
 
@@ -83,9 +106,8 @@ std::ostream& operator<<(std::ostream& os, Serving& serving) {
     os << serving.get_container();
     for (Flavor s : serving.get_flavors()) os << std::endl << s;
     for (Topping t : serving.get_toppings()) os << std::endl << t;
-    os << std::endl << std::setw(50) << "----------------------------" 
+    os << std::endl << std::setw(50) << "----------------------------"
     << std::endl << std::setw(40) << "Serving Total: $ " << serving.get_total_retail_price() << endl;
     return os;
 */
 }
-
--- serving.h
+++ serving.h
@@ -1,19 +1,22 @@
 #ifndef SERVING_H
 #define SERVING_H
 #include <vector>
+#include <fstream>
 #include "items.h"
 
 using namespace std;
 
 class Serving{
 	private:
-		Containr _container; 
+		Containr _container{"", "", 0, 0, "", 0};
 		vector<Flavor>  _flavors;
 		vector<Topping> _toppings;
 		vector<int> _top_kind;
 
 	public:
 		Serving(Containr container);
+		Serving(std::istream& ist);
+    	void save(std::ostream& ost);
 		void set_topping(Topping topping, int kind);
 		void set_flavor(Flavor flavor);
 
@@ -22,11 +25,9 @@ class Serving{
 		vector<Topping> get_toppings() ;
 		vector<int> get_top_kind() ;
 		double get_total_retail_price() ;
-		double get_total_wholesale_price();		
+		double get_total_wholesale_price();
 };
 
 std::ostream& operator<<(std::ostream& os, const Serving& serving);
 
 #endif
-
-	
